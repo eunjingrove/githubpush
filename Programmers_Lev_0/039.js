a2670da444aa042a368cc2d8c947f28f1ad0ecb1
eunjin.Park <138556026+eunjingrove@users.noreply.github.com>
2023-08-15 21:41:13 +0900

    최댓값 만들기 (2)

---
+++ Programmers_Lev_0/039.js
@@ -0,0 +1,12 @@
+// 최댓값 만들기 (2)
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+    var answer = [];
+    for(let i=0; i<numbers.length; i++) {
+        for(let k=i+1; k<numbers.length; k++) {
+            answer.push(numbers[i]*numbers[k]);
+        }
+    }
+    return Math.max(...answer);
+}
