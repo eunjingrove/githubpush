e819dddd7dcf13b82956f6333ff9b86156b7fffa
eunjin.Park <ok94777@naver.com>
2023-09-05 17:26:23 +0900

    vendingMachine

--- JS/20230904/vending/js/colaGenerator.js
+++ JS/20230904/vending/js/colaGenerator.js
@@ -3,7 +3,7 @@
 
 class ColaGenerator {
     constructor() {
-        // 초기화 값
+        // 콜라 객체를 초기화
         this.itemList = document.querySelector('.cola-list');
     }
 
@@ -13,7 +13,7 @@ class ColaGenerator {
     }
 
     async loadData() {
-        // 데이터를 로드하는 함수
+        // 콜라 관련 데이터를 로드
         try {
             const response = await fetch('./items.json');
             console.log(response);
@@ -36,14 +36,16 @@ class ColaGenerator {
     //   </button>
     // </li>
 
+    // 콜라의 템플릿 코드 
     colaFactory(data) {
         const docFrag = new DocumentFragment();
         data.forEach((el) => {
             const item = document.createElement('li');
-            const itemTemplate = `<button type="button" class="btn-cola">
-            <img src="./img/cola-cool.png" alt="" />
-            <span class="cola-name">Cool_Cola</span>
-            <strong class="cola-price">1000원</strong>
+            const itemTemplate = `<button type="button" class="btn-cola" data-item="${el.name}" 
+            data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+            <img src="./img/${el.img}" alt="${el.name}" />
+            <span class="cola-name">${el.name}</span>
+            <strong class="cola-price">${el.cost}원</strong>
           </button>`
             item.innerHTML = itemTemplate;
             docFrag.append(item);
