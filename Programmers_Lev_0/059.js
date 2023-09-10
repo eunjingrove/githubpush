b07dbd9224850f53ac2bc2efcb0863fbe3a3f32f
eunjin.Park <138556026+eunjingrove@users.noreply.github.com>
2023-09-10 20:35:13 +0900

    한 번만 등장한 문자

---
+++ Programmers_Lev_0/059.js
@@ -0,0 +1,10 @@
+// 한 번만 등장한 문자
+// 문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요. 한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+
+function solution(s) {
+     return [...s].filter((item)=> {
+        let arr = [...s];
+        arr.splice(arr.indexOf(item),1);
+        return !arr.includes(item);
+    }).sort().join('');
+}
