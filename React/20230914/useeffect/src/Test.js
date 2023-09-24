5d6d1489b25b951682c801e2bf1ec70196b7fdf8
eunjin.Park <ok94777@naver.com>
2023-09-24 17:51:42 +0900

    react test

---
+++ React/20230914/useeffect/src/Test.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
+function Time() {
+    const [today, setToday] = useState(new Date());
+    const [hour, setHour] = useState(today.getHours())
+    const [min, setMin] = useState(today.getMinutes())
+    const [sec, setSec] = useState(today.getSeconds())
+
+    useEffect(() => {
+        let time = setInterval(() => {
+            const t = new Date();
+            setToday(t)
+            setHour(t.getHours());
+            setMin(t.getMinutes());
+            setSec(t.getSeconds());
+        }, 1000)
+        return () => {
+            clearInterval(time);
+        }
+    })
+
+
+
+
+
+    return (
+        <>
+            <h1>시간: {hour}시 {min}분 {sec}초</h1>
+        </>
+    )
+}
+
+function Test() {
+    return (
+        <div>
+            <Time />
+        </div>
+    )
+}
+
+export default Test
--- React/20230914/useeffect/src/index.js
+++ React/20230914/useeffect/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import Test from './Test'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<Test />);
