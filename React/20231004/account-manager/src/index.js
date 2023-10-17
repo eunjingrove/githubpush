18dc338df4d846a07fadde3f54c15459c667fa16
eunjin.Park <ok94777@naver.com>
2023-10-17 22:24:12 +0900

    react test

--- React/20231004/account-manager/src/index.js
+++ React/20231004/account-manager/src/index.js
@@ -4,14 +4,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import store from './store'
 import { Provider } from 'react-redux'
-import Test from './Test'
-
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
     <Provider store={store}>
-        <Test />
+        <App />
     </Provider>
 
 );
