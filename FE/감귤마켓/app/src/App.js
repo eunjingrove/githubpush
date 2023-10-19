4cde13d650c23b50d5d583003441be499a1ca22c
eunjin.Park <ok94777@naver.com>
2023-10-19 20:34:13 +0900

    감귤마켓 axios api test

--- FE/감귤마켓/app/src/App.js
+++ FE/감귤마켓/app/src/App.js
@@ -1,41 +1,47 @@
 import { useState } from "react";
 import JoinPage from "./Components/JoinPage";
 import LoginPage from "./Components/LoginPage";
+import Signup from "./Components/Signup";
 
 function App() {
-  const [accountname, setAccountName] = useState('')
-  const [page, setPage] = useState(true)
+  const [accountname, setAccountName] = useState("");
+  const [page, setPage] = useState(true);
   const handlePage = () => {
     setPage((prevPage) => {
-      return !prevPage
-    })
-  }
+      return !prevPage;
+    });
+  };
 
   const getMyinfo = async () => {
-    // 토큰 가져오기 
-    const token = localStorage.getItem('token')
+    // 토큰 가져오기
+    const token = localStorage.getItem("token");
     console.log(token);
-    const reqUrl = 'https://api.mandarin.weniv.co.kr/user/myinfo'
+    const reqUrl = "https://api.mandarin.weniv.co.kr/user/myinfo";
 
     const res = await fetch(reqUrl, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
+        Authorization: `Bearer ${token}`,
+      },
+    });
     const json = await res.json();
-    console.log(json)
-    setAccountName(json.user.accountname)
-
-  }
+    console.log(json);
+    setAccountName(json.user.accountname);
+  };
 
   return (
     <div>
-      <button type="button" onClick={getMyinfo}>내 정보 불러오기</button>
+      <button type="button" onClick={getMyinfo}>
+        내 정보 불러오기
+      </button>
       <ul>
         <li>{accountname}</li>
       </ul>
-      {page ? <LoginPage handlePage={handlePage} /> : <JoinPage handlePage={handlePage} />}
+      {page ? (
+        <LoginPage handlePage={handlePage} />
+      ) : (
+        <Signup handlePage={handlePage} />
+      )}
     </div>
   );
 }
